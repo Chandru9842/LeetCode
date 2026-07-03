af2654a5270994721d3ecd056d230fb7e563846f
CHANDRU M <chandrumohan550@gmail.com>
2026-07-03 15:38:09 +0530

    Time: 20 ms (96.01%), Space: 83 MB (63.98%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] sortArray(int[] arr) {
+        // int n=arr.length;
+        // for(int i=0;i<n-1;i++){
+        //     int mid=i;
+        //     for(int j=i;j<n;j++){
+        //         if(arr[j]<arr[mid]){
+        //             mid=j;
+        //         }
+        //     }
+        //     int temp=arr[mid];
+        //     arr[mid]=arr[i];
+        //     arr[i]=temp;
+        // }
+        // return arr;
+        int left=0;
+        // int right=
+        Arrays.sort(arr);
+        return arr;
+        
+    }
+}
