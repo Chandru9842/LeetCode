2b8e1db5185e455fcc827f81ea337a86bcd149cf
CHANDRU M <chandrumohan550@gmail.com>
2026-07-04 09:40:51 +0530

    Time: 13 ms (14.41%), Space: 52.4 MB (16.64%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -4,12 +4,27 @@ class Solution {
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[i].length;j++){
                     char c=board[i][j];
-                    if(c!='.'){
-                        if(!set.add(c+"in r"+i)||!set.add(c+"in c"+j)||!set.add(c+"in box"+(i/3)+"-"+(j/3))){
-                            return false;
-                        }
-                        
+                    if(c=='.'){
+                        continue;
                     }
+                   String row=c+"in row"+i;
+                    String col=c+"in col"+j;
+                    String box=c+"in the box"+(i/3)+"-"+(j/3);
+
+                   if(set.contains(row)){
+                    return false;
+                   }
+                   set.add(row);
+                //    String col=c+"in col"+j;
+                   if(set.contains(col)){
+                    return false;
+                   }
+                   set.add(col);
+                //    String box=c+"in the box"+(i/3)+"-"+(j/3);
+                   if(set.contains(box)){
+                    return false;
+                   }
+                   set.add(box);
 
         }
         }
