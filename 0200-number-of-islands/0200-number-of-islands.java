988413bc30ad38b49ff550ad6665fbd3d325fcab
CHANDRU M <chandrumohan550@gmail.com>
2026-07-07 10:50:25 +0530

    Time: 4 ms (46.47%), Space: 51.6 MB (81.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,13 +1,14 @@
 class Solution {
-    void dfs(int i,int j,char[][]grid,int [][]vis){
-        if(i<0||i>=grid.length||j<0||j>=grid[0].length||grid[i][j]=='0'||vis[i][j]==1){
-            return;
+    void dfs(int row,int col,char[][]grid,int [][]vis,int[] delrow,int[] delcol){
+       vis[row][col]=1;
+       for(int i=0;i<4;i++){
+        int nrow=row+delrow[i];
+        int ncol=col+delcol[i];
+        if(nrow<0||nrow>=grid.length||ncol<0||ncol>=grid[0].length||grid[nrow][ncol]=='0'||vis[nrow][ncol]==1){
+            continue ;
         }
-        vis[i][j]=1;
-        dfs(i+1,j,grid,vis);
-        dfs(i,j+1,grid,vis);
-        dfs(i-1,j,grid,vis);
-        dfs(i,j-1,grid,vis);
+        dfs(nrow,ncol,grid,vis,delrow,delcol);
+       }
     }
     public int numIslands(char[][] grid) {
         if(grid.length==0){
@@ -15,12 +16,14 @@ class Solution {
         }
         int n=grid.length;
         int count=0;
+        int[]delrow=new int[]{-1,0,1,0};
+        int[]delcol=new int[]{0,1,0,-1};
         int vis[][]=new int[n][grid[0].length];
         for(int i=0;i<n;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]=='1'&&vis[i][j]==0){
                     count++;
-                    dfs(i,j,grid,vis);
+                    dfs(i,j,grid,vis,delrow,delcol);
                     
 
                 }
