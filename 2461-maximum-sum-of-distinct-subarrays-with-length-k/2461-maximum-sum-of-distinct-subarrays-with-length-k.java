7f4a004ab974327f88730bfa5a83454dab96a5ea
CHANDRU M <chandrumohan550@gmail.com>
2026-09-04 12:39:09 +0530

    Time: 9 ms (98.16%), Space: 85.1 MB (97.16%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,33 +1,33 @@
 class Solution {
     // public long maximumSubarraySum(int[] arr, int k) {
-    //     int l=0;
-    //     int j=0;
-    //     long max=0,sum=0;
-    //     // Arrays.sort(arr);
-    //     int[] freq = new int[100001];
-    //     for(int r=0;r<arr.length;r++){
-    //         sum+=arr[r];
-    //         freq[arr[r]]++;
-    //         if(r-l+1>k){
-    //             sum-=arr[l];
-    //             freq[arr[l]]--;
-    //             l++;
-    //         }
-    //         if(r-l+1==k){
-    //            boolean found=true;
-    //            for(int i=l;i<=r;i++){
-    //             if(freq[arr[i]]>1){
-    //                 found=false;
-    //             }
-    //            }
-    //            if(found){
-    //             max=Math.max(max,sum);
+        // int l=0;
+        // int j=0;
+        // long max=0,sum=0;
+        // // Arrays.sort(arr);
+        // int[] freq = new int[100001];
+        // for(int r=0;r<arr.length;r++){
+        //     sum+=arr[r];
+        //     freq[arr[r]]++;
+        //     if(r-l+1>k){
+        //         sum-=arr[l];
+        //         freq[arr[l]]--;
+        //         l++;
+        //     }
+        //     if(r-l+1==k){
+        //        boolean found=true;
+        //        for(int i=l;i<=r;i++){
+        //         if(freq[arr[i]]>1){
+        //             found=false;
+        //         }
+        //        }
+        //        if(found){
+        //         max=Math.max(max,sum);
 
-    //            }
-    //         }
+        //        }
+        //     }
            
-    //     }
-    //     return max;
+        // }
+        // return max;
         public long maximumSubarraySum(int[] arr, int k) {
             int dist=0;
             long max=0;
@@ -59,4 +59,26 @@ class Solution {
             return max;
         
     }
+    // public long maximumSubarraySum(int[]arr,int k){
+    //     Set<Integer>set=new HashSet<>();
+    //     int l=0;
+    //     long sum=0;
+    //     long max=0;
+    //     for(int r=0;r<arr.length;r++){
+    //         sum+=arr[r];
+    //         set.add(arr[r]);
+    //         if(r-l+1>k){
+    //             sum-=arr[l];
+    //             set.remove(arr[l]);
+    //             l++;
+    //         }
+    //         if(r-l+1==k){
+    //             if(set.size()==k){
+    //                 max=Math.max(max,sum);
+    //             }
+    //         }
+    //     }
+    //     return max;
+
+    // }
 }
