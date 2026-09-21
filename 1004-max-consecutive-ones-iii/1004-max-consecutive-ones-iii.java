922120a865158588cc2411dab67b9ef7f65a4d7a
CHANDRU M <chandrumohan550@gmail.com>
2026-09-21 18:49:23 +0530

    Time: 3 ms (95.92%), Space: 52 MB (87.99%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -37,6 +37,23 @@ class Solution {
         //     max=Math.max(max,end-start+1);//5
         // }
         // return max; 
+        // int max=0;
+        // int zeros=0;
+        // int l=0;
+        // for(int r=0;r<nums.length;r++){
+        //     if(nums[r]==0){
+        //         zeros++;
+        //     }
+        //     while(zeros>k){
+        //         if(nums[l]==0){
+        //             zeros--;
+        //         }
+        //         l++;
+        //     }
+        //     max=Math.max(max,r-l+1);
+        // }
+        // return max;
+
         int max=0;
         int zeros=0;
         int l=0;
@@ -53,7 +70,6 @@ class Solution {
             max=Math.max(max,r-l+1);
         }
         return max;
-
 
 
 
