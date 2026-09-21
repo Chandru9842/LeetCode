bf8a28edc8b03c3248983726ed9dd1387245966d
CHANDRU M <chandrumohan550@gmail.com>
2026-09-21 19:07:14 +0530

    Time: 84 ms (10.76%), Space: 47.5 MB (84.3%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -56,9 +56,9 @@ class Solution {
             map.put(s.charAt(r),map.getOrDefault(s.charAt(r),0)+1);
             while(map.get(s.charAt(r))>1){
                 map.put(s.charAt(l),map.get(s.charAt(l))-1);
-                if(map.get(s.charAt(l))==0){
-                    map.remove(s.charAt(l));
-                }
+                // if(map.get(s.charAt(l))==0){
+                //     map.remove(s.charAt(l));
+                // }
                 l++;
 
             }
